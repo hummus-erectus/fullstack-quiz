3e0e633a87c286a1db70dfe6ab40246ecc06e857
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-05-25 13:26:54 +0900

    Create questionsReducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import notificationReducer from './reducers/notificationReducer'
 import quizReducer from './reducers/quizReducer'
+import questionsReducer from './reducers/questionsReducer'
 import userReducer from './reducers/userReducer'
 import usersReducer from './reducers/usersReducer'
 
@@ -8,6 +9,7 @@ const store = configureStore({
   reducer: {
     notification: notificationReducer,
     quiz: quizReducer,
+    question: questionsReducer,
     user: userReducer,
     users: usersReducer
   }

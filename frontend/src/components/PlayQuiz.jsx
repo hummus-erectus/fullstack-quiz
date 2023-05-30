de229cb216b4a9acb4cd34a87c2b598a7876455f
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-05-30 11:25:00 +0900

    Fix quiz behaviour

--- frontend/src/components/PlayQuiz.jsx
+++ frontend/src/components/PlayQuiz.jsx
@@ -1,103 +1,145 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Togglable from './Togglable'
+import { Button } from './styles/Button.styled'
 
 const PlayQuiz = ({ questions }) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+  const [score, setScore] = useState(0)
+  const [showFeedback, setShowFeedback] = useState(false)
   const [userAnswers, setUserAnswers] = useState([])
-  const [showResults, setShowResults] = useState(false)
+  const [shuffledOptions, setShuffledOptions] = useState([])
+
+  useEffect(() => {
+    if(currentQuestionIndex !== -1){
+      shuffleOptions()
+    }
+  }, [currentQuestionIndex])
 
   const currentQuestion = questions[currentQuestionIndex]
+  const isLastQuestion = currentQuestionIndex === questions.length - 1
 
-  const handleAnswerClick = (selectedAnswerIndex) => {
-    const answerIsCorrect = selectedAnswerIndex === currentQuestion.correctAnswer
-    setUserAnswers([...userAnswers, { question: currentQuestion, isCorrect: answerIsCorrect }])
+  const shuffleOptions = () => {
+    const options = [...currentQuestion.options]
+    for (let i = options.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [options[i], options[j]] = [options[j], options[i]]
+    }
+    setShuffledOptions(options)
   }
 
-  const handleNextQuestionClick = () => {
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1)
+  const handleAnswerClick = (optionId) => {
+    const isCorrect = optionId === currentQuestion.correctAnswer
+    setUserAnswers((prevAnswers) => [
+      ...prevAnswers,
+      { questionId: currentQuestion._id, optionId, isCorrect },
+    ])
+    setScore((prevScore) => (isCorrect ? prevScore + 1 : prevScore))
+    setShowFeedback(true)
+  }
+
+  const handleNextQuestion = () => {
+    setShowFeedback(false)
+    if (isLastQuestion) {
+      setCurrentQuestionIndex(-1) // Set to -1 to indicate showing result screen
     } else {
-      setShowResults(true)
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1)
+      shuffleOptions()
     }
   }
 
-  const handleRetryClick = () => {
+  const handleRetryQuiz = () => {
     setCurrentQuestionIndex(0)
+    setScore(0)
     setUserAnswers([])
-    setShowResults(false)
+    setShowFeedback(false)
+    setUserAnswers([])
   }
 
-  const renderQuestion = () => {
-    // Randomize answer options
-    const randomizedOptions = shuffleArray(currentQuestion.options)
-
-    const options = randomizedOptions.map((option, index) => {
-      const optionLetter = String.fromCharCode(65 + index) // A, B, C, D
-      return (
-        <button key={index} onClick={() => handleAnswerClick(index)}>
-          {optionLetter}. {option}
-        </button>
-      )
-    })
-
-    const shuffleArray = (array) => {
-      const shuffledArray = [...array]
-      for (let i = shuffledArray.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
-      }
-      return shuffledArray
-    }
-
-    return (
-      <div>
-        <h2>{currentQuestion.content}</h2>
-        {options}
-      </div>
-    )
+  const renderOptions = () => {
+    return shuffledOptions.map((option) => (
+      <button
+        key={option._id}
+        onClick={() => handleAnswerClick(option.optionId)}
+        disabled={showFeedback}
+      >
+        {option.content}
+      </button>
+    ))
   }
 
   const renderFeedback = () => {
-    const lastAnswer = userAnswers[userAnswers.length - 1]
-    const isCorrect = lastAnswer.isCorrect
-    const correctAnswer = lastAnswer.question.options[lastAnswer.question.correctAnswer]
+    const { options, correctAnswer } = currentQuestion
+    const selectedAnswer = userAnswers[userAnswers.length - 1]
+    const isCorrect = selectedAnswer.isCorrect
 
     return (
       <div>
         <p>{isCorrect ? 'Correct!' : 'Incorrect!'}</p>
-        {!isCorrect && <p>The correct answer is: {correctAnswer}</p>}
-        <button onClick={handleNextQuestionClick}>Next Question</button>
+        {!isCorrect && (
+          <p>
+            The correct answer is:{' '}
+            {options.find((option) => option.optionId === correctAnswer).content}
+          </p>
+        )}
+        {isLastQuestion ? (
+          <button onClick={handleNextQuestion}>See Results</button>
+        ) : (
+          <button onClick={handleNextQuestion}>Next Question</button>
+        )}
       </div>
     )
   }
 
-  const renderResults = () => {
+  const renderResult = () => {
     const totalQuestions = questions.length
-    const totalCorrectAnswers = userAnswers.filter(answer => answer.isCorrect).length
-    const score = (totalCorrectAnswers / totalQuestions) * 100
-
-    const incorrectQuestions = userAnswers.filter(answer => !answer.isCorrect).map(answer => answer.question)
-
-    const incorrectQuestionsList = incorrectQuestions.map((question, index) => (
-      <li key={index}>{question.content}</li>
-    ))
+    const incorrectAnswers = userAnswers.filter((answer) => !answer.isCorrect)
 
     return (
       <div>
-        <h2>Results</h2>
-        <p>Your score: {score}%</p>
-        <button onClick={handleRetryClick}>Retry Quiz</button>
-        <button>Go Back to QuizView</button>
-        <h3>Incorrect Questions</h3>
-        <ul>{incorrectQuestionsList}</ul>
+        <p>Your score: {score}/{totalQuestions}</p>
+
+        <Button onClick={handleRetryQuiz}>Retry Quiz</Button>
+        {incorrectAnswers.length > 0 && (
+          <div>
+            <Togglable buttonLabel="See Incorrect Answers">
+              {incorrectAnswers.map((answer) => {
+                const question = questions.find((q) => q._id === answer.questionId)
+                const selectedOption = question.options.find(
+                  (option) => option.optionId === answer.optionId
+                )
+                const correctOption = question.options.find(
+                  (option) => option.optionId === question.correctAnswer
+                )
+
+                return (
+                  <div key={question._id}>
+                    <h3>Question: {question.content}</h3>
+                    <p>Your answer: {selectedOption.content}</p>
+                    <p>Correct answer: {correctOption.content}</p>
+                  </div>
+                )
+              })}
+            </Togglable>
+
+          </div>
+        )}
       </div>
     )
   }
 
+
   return (
     <div>
-      {!showResults && renderQuestion()}
-      {showResults && renderResults()}
-      {userAnswers.length > 0 && !showResults && renderFeedback()}
+      {currentQuestionIndex === -1 ? (
+        renderResult()
+      ) : (
+        <>
+          <h2>Question {currentQuestionIndex + 1}</h2>
+          <p>{currentQuestion.content}</p>
+          {renderOptions()}
+          {showFeedback && renderFeedback()}
+        </>
+      )}
     </div>
   )
 }

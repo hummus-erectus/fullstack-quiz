31190a2de2f7bc79f868bd0c1acedeb5feba9539
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-05-30 14:00:43 +0900

    Update users.js
    Use regex for password validation

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,44 +2,49 @@ const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/
+
 usersRouter.post('/', async (request, response) => {
-    const { username, name, password } = request.body
+  const { username, name, password } = request.body
 
-    if (!password) {
-        return response.status(400).json({ error: 'You must provide a password' })
-    }
+  if (!password) {
+    return response.status(400).json({ error: 'You must provide a password' })
+  }
 
-    if (password.length < 3) {
-        return response.status(400).json({ error: 'Password must be at least 3 characters' })
-    }
-
-
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
-
-    const user = new User({
-        username,
-        name,
-        passwordHash,
+  if (!passwordRegex.test(password)) {
+    return response.status(400).json({
+      error:
+        'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit',
     })
+  }
 
-    try {
-        const savedUser = await user.save()
-        response.status(201).json(savedUser)
-    } catch (error) {
-        if (error.name === 'ValidationError') {
-            if (error.errors.username.kind === 'unique') {
-                return response.status(409).json({ error: 'Username is already taken' })
-            }
-            return response.status(400).json({ error: error.message })
-        }
-        throw error
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  try {
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      if (error.errors.username.kind === 'unique') {
+        return response.status(409).json({ error: 'Username is already taken' })
+      }
+      return response.status(400).json({ error: error.message })
     }
+    throw error
+  }
 })
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
-    response.json(users)
+  const users = await User.find({})
+  response.json(users)
 })
 
 module.exports = usersRouter

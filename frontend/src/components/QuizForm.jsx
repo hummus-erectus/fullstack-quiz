1e73808a93d93e6207f757f5e2023c74fef3fa8c
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-06-06 12:41:00 +0900

    Update QuizForm.jsx
    Add 'Description' field

--- frontend/src/components/QuizForm.jsx
+++ frontend/src/components/QuizForm.jsx
@@ -4,14 +4,18 @@ import { Form } from './styles/Form.styled'
 
 const QuizForm = ({ createQuiz }) => {
   const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
 
   const addQuiz = async (event) => {
     event.preventDefault()
     createQuiz({
       title: title,
+      description: description,
     })
 
     setTitle('')
+    setDescription('')
   }
 
   return (
@@ -20,13 +24,24 @@ const QuizForm = ({ createQuiz }) => {
 
       <form onSubmit={addQuiz}>
         <div className='input'>
-          title:
+          Title:
           <input
             id="titleInput"
             type="text"
             value={title}
             name="Title"
+            required
             onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div className='input'>
+          Description:
+          <input
+            id="descriptionInput"
+            type="text"
+            value={description}
+            name="Description"
+            onChange={({ target }) => setDescription(target.value)}
           />
         </div>
         <Button type="submit" className="submitButton">

082558f9e99040a7b369d94344dcc7712fe7ab23
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-06-16 16:41:54 +0900

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import notificationReducer from './reducers/notificationReducer'
 import quizReducer from './reducers/quizReducer'
-import questionsReducer from './reducers/questionsReducer'
 import userReducer from './reducers/userReducer'
 import usersReducer from './reducers/usersReducer'
 import activeQuizReducer from './reducers/activeQuizReducer'
@@ -10,7 +9,6 @@ const store = configureStore({
   reducer: {
     notification: notificationReducer,
     quiz: quizReducer,
-    question: questionsReducer,
     user: userReducer,
     users: usersReducer,
     activeQuiz: activeQuizReducer,

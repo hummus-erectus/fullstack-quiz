5026d38e743791e293ec2b7ed021c97bf562685b
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-06-22 16:34:54 +0900

    Update QuizView.styled.js

--- frontend/src/components/styles/QuizView.styled.js
+++ frontend/src/components/styles/QuizView.styled.js
@@ -1,66 +1,66 @@
 import styled from 'styled-components'
 
 export const StyledQuizView = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .description {
+    font-weight: 600;
+  }
+
+  .username {
+    font-weight: 600;
+    color: ${({ theme }) => theme.colors.primaryContent || '#000'};
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .commentsContainer {
+    margin-top: 30px;
+  }
+
+  .userComment {
+    background-color: ${({ theme }) => theme.colors.primary || '#FFF'};
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    padding: 10px;
+    margin-bottom: 20px;
+    margin-left: 20px;
+    max-width: 70%;
+  }
+
+  .noComments {
+    margin-top: 0px;
+  }
+
+  form {
     display: flex;
     flex-direction: column;
+    gap: 10px;
+  }
 
-    .description {
-        font-weight: 600;
-    }
+  input {
+    margin-left: 20px;
+    width: 70%;
+    height: 2rem;
+  }
 
-    .username {
-        font-weight: 600;
-        color: ${({ theme }) => theme.colors.primaryContent || '#000'};
-        text-decoration: none;
-    }
+  Button {
+    margin-top: 15px;
+    align-self: flex-start;
+  }
 
-    ul {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-    }
+  .openButtonContainer {
+    display: inline;
+  }
 
-    .commentsContainer {
-        margin-top: 30px;
-    }
-
-    .userComment {
-        background-color: ${({ theme }) => theme.colors.primary || '#FFF'};
-        border: 1px solid #ddd;
-        border-radius: 10px;
-        padding: 10px;
-        margin-bottom: 20px;
-        margin-left: 20px;
-        max-width: 70%;
-    }
-
-    .noComments {
-        margin-top: 0px;
-    }
-
-    form {
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
-    }
-
-    input {
-        margin-left: 20px;
-        width: 70%;
-        height: 2rem;
-    }
-
-    Button {
-        margin-top: 15px;
-        align-self: flex-start;
-    }
-
-    .openButtonContainer {
-        display: inline;
-    }
-    .clickableIcon {
-        cursor: pointer;
-        font-size: 1.2em;
-    }
-
+  .clickableIcon {
+    cursor: pointer;
+    font-size: 1.2em;
+  }
 `

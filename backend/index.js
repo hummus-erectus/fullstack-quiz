fc5708df50b0996c57fb71ba3d49659fdc454fd6
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-06-29 21:00:43 +0900

    Prepare for deploy via fly.io

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-app.listen(config.PORT, () => {
+const port = config.PORT || 3003
+
+app.listen(port, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
